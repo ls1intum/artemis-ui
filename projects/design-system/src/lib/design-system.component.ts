ed591c6f726a166be33af652902d2aaa042f7fac
bgeisb <benedikt@geisberger.biz>
2023-11-09 15:36:46 +0100

    Initial commit

---
+++ projects/design-system/src/lib/design-system.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-design-system',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      design-system works!
+    </p>
+  `,
+  styles: ``
+})
+export class DesignSystemComponent {
+
+}
---
+++ projects/design-system/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of design-system
+ */
+
+export * from './lib/design-system.service';
+export * from './lib/design-system.component';
